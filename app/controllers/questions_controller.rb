d31deb76586eb5852782fd40304f7ace8f21497b
BrunoRanieri <ranieri.bruno113@gmail.com>
2024-02-21 15:35:45 -0300

    stupid challange

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,8 @@ class QuestionsController < ApplicationController
           @answer = "Great!"
         elsif @question.end_with?("?")
             @answer = "Silly question, get dressed and go to work!"
-        else @answer = "I don't care, get dressed and go to work!"
+        else 
+            @answer = "I don't care, get dressed and go to work!"
+        end
     end
 end
